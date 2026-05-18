52ec9babc1f3d5c42d03ec8f767ecee54d61e12b
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-05-17 20:16:11 -0500

    bump version, 1.0.0 => 2.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: MelonInfo(typeof(Mapper), "MSZDialougeMapper", "1.0.0", "Gameknight963")]
+[assembly: MelonInfo(typeof(Mapper), "MSZDialougeMapper", "2.0.0", "Gameknight963")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
